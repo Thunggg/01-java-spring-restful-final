c43118aaa6dc62a245e873fdf551e57b5a2e917e
Thunggg <thuannguyen20041028@gmail.com>
2025-01-12 17:46:09 +0700

    them user

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/User.java
@@ -0,0 +1,59 @@
+package vn.hoidanit.jobhunter.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+
+    // Getter for id
+    public long getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for email
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter for email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter for password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
